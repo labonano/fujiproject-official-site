9eb627e0d6c0267700b1169e09d7fe14ece6385e
labonano <m210114@mict.ed.jp>
2024-11-01 16:46:37 +0900

    Update blackdesuka.js

--- security/blackdesuka.js
+++ security/blackdesuka.js
@@ -1,24 +1,20 @@
-async function checkIPWithGAS() {
-  try {
-    // クライアントの IP を取得
-    const ipResponse = await fetch('https://api64.ipify.org?format=json');
-    const { ip } = await ipResponse.json();
+    async function checkIPWithGAS() {
+      try {
+        const ipResponse = await fetch('https://api64.ipify.org?format=json');
+        const { ip } = await ipResponse.json();
 
-    // GAS Web アプリに IP を送信
-    const gasUrl = `https://script.google.com/a/macros/mict.ed.jp/s/AKfycbx8_qSaAOumIIop6g1G0TqCUGJ1iKbxYePkP-1nCT_V9MwVV5ldPL0uHJpfFsZTieY/exec?ip=${ip}`;
-    const response = await fetch(gasUrl);
-    const data = await response.json();
+        const gasUrl = `https://script.google.com/a/macros/mict.ed.jp/s/AKfycbx8_qSaAOumIIop6g1G0TqCUGJ1iKbxYePkP-1nCT_V9MwVV5ldPL0uHJpfFsZTieY/exec?ip=${ip}`;
+        const response = await fetch(gasUrl);
+        const data = await response.json();
 
-    if (data.blacklisted) {
-      // ブラックリスト入りならリダイレクト
-      window.location.href = 'https://fujiproject-off1cial.f5.si/403.html';
-    } else {
-      console.log('アクセス許可されています');
+        if (data.blacklisted) {
+          window.location.href = 'https://fujiproject-off1cial.f5.si/403.html';
+        } else {
+          alert('アクセス許可されています');
+        }
+      } catch (error) {
+        alert('エラーが発生しました: ' + error.message);
+      }
     }
-  } catch (error) {
-    console.error('エラーが発生しました:', error);
-  }
-}
 
-// ページ読み込み時に IP チェックを実行
-checkIPWithGAS();
+    checkIPWithGAS();
